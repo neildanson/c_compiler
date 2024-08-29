1bd4c32f7343827c4c6e6816fb2cf8d011201759
Neil Danson <neil.danson@gmail.com>
2024-08-29 15:08:54 +0100

    running instructions and add clap for command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::io::{BufWriter, Write};
 
+//gcc asm.s
+//./a.out
+//echo $?
+
 fn main() -> std::io::Result<()> {
-    //write a file with string contents
     let file = std::fs::File::create("asm.s")?;
     let mut buff = BufWriter::new(file);
-    write!(buff, "Hello world")?;
+    write!(buff, ".globl main
+main:
+    movl $2, %eax
+    ret
+")?;
     Ok(())
 }
