cc30a4b920f68bcfd54d544b581bde0190a1377b
Neil Danson <neil.danson@gmail.com>
2024-08-30 10:52:06 +0100

    Refactor parse function to handle different token patterns

--- src/ast.rs
+++ src/ast.rs
@@ -4,6 +4,11 @@ use crate::token::Token;
 pub struct Ast;
 
 
-pub fn parse(_tokens : Vec<Token>) -> Ast {
-    Ast
+pub fn parse(tokens : Vec<Token>) -> Ast {
+    match tokens.as_slice() {
+        &[] => Ast,
+        &[Token::Int, Token::Identifier(_), Token::LParen, Token::RParen, Token::LBrace, Token::RBrace] => Ast,
+        _ => panic!("Failed to parse {:?}", tokens),
+    }
+
 }
