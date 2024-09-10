688897364f5c99ab288f08497728551a5278baba
Neil Danson <neil.danson@gmail.com>
2024-09-10 13:24:02 +0100

    time program

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 cargo test
-cargo run -- --codegen main.c --S
 cargo build
 
 ../writing-a-c-compiler-tests/test_compiler ./target/debug/c_compiler --chapter 3 --stage codegen --bitwise
 
+cargo run -- --codegen main.c --S
+
 gcc a.s
-./a.out
+time ./a.out
 echo "My Code : $?"
 
 gcc -o b.out main.c
-./b.out
+time ./b.out
 echo "GCC Code : $?"
