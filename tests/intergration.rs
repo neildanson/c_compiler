22779f38d0678657a95cd4f2e3a9b6b6b4020f8d
Neil Danson <neil.danson@gmail.com>
2024-09-24 16:25:33 +0100

    Fix integration tests

--- tests/intergration.rs
+++ tests/intergration.rs
@@ -2,14 +2,16 @@ use std::process::{Command, ExitStatus};
 
 use c_compiler::*;
 
-fn run_gcc(input: &str, output : &str) -> ExitStatus {
+fn run_gcc(input: &str, output : &str) -> bool {
     let output = Command::new("gcc")
-        .arg(input)
-        .arg(format!("-o {}", output))
+        .args([input, "-o", &output])
         .output()
         .expect("failed to execute process");
 
-    output.status
+    println!("status: {}", output.status);
+    let err = String::from_utf8_lossy(&output.stderr);
+    println!("stderr: {}", err);
+    output.status.success()
 }
 
 fn run_output(filename: &str) -> ExitStatus {
@@ -24,18 +26,19 @@ fn run_test(input: &str) {
     let path = "tests";
     let c_file = format!("{}/{}.c", path, input);
     let s_file = format!("{}/temp/{}.s", path, input);
-    let out_file = format!("{}/temp/my_{}.out", path, input);
+    let my_out_file = format!("{}/temp/my_{}.out", path, input);
+    let gcc_out_file = format!("{}/temp/gcc_{}.out", path, input);
+    
     let generate_assembly = codegen(&c_file).unwrap();
     write_asm(&s_file, &generate_assembly).unwrap();
-    let my_status = run_gcc(&s_file, &out_file);
-    let my_result = run_output(&out_file);
+    let my_status = run_gcc(&s_file, &my_out_file);
+    let gcc_status = run_gcc(&c_file, &gcc_out_file);
+    assert_eq!(true, my_status, "GCC Compilation failed (my assembler)");
+    assert_eq!(true, gcc_status, "GCC Compilation failed");
 
-    let out_file = format!("{}/temp/gcc_{}.out", path, input);
-    let gcc_status = run_gcc(&c_file, &out_file);
-    let gcc_result = run_output(&out_file);
+    let my_result = run_output(&my_out_file);
+    let gcc_result = run_output(&gcc_out_file);
 
-
-    assert_eq!(my_status, gcc_status, "GCC Compilation failed");
     assert_eq!(my_result, gcc_result, "App difference");
 }
 
