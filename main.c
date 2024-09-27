5f3361aaecdc69b883fa5aadc6815061789b20a3
Neil Danson <neil.danson@gmail.com>
2024-09-27 16:11:54 +0100

    Run code more!

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 int main(void) {
     int k = 1;
 
-    for (int i = 0; i < 10; i = i + 1) {
-        if (k > 5) {
-            k = k + 3;
-            if (k % 2 == 0) {
-                continue;
-            }
-        }
-        k = k + 2;
+    for (int i = 0; i < 100000000; i = i + 1) {
+        k = k + 1;
     }
+    
     return k;
 }
