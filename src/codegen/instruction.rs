0946e240636f64cac75cbd656f338250a18ba77e
Neil Danson <neil.danson@gmail.com>
2024-10-09 11:49:02 +0100

    Improve to match gcc asm output

--- src/codegen/instruction.rs
+++ src/codegen/instruction.rs
@@ -140,14 +140,6 @@ fn convert_function_call(
         instructions.push(Instruction::AllocateStack(stack_padding));
     }
 
-    for (i, arg) in register_args.iter().enumerate() {
-        let assembly_arg = (*arg).clone().into();
-        instructions.push(Instruction::Mov {
-            src: assembly_arg,
-            dst: i.into(),
-        });
-    }
-
     for arg in stack_args.iter().rev() {
         let assembly_arg = (*arg).clone().into();
         match assembly_arg {
@@ -162,6 +154,14 @@ fn convert_function_call(
                 instructions.push(Instruction::Push(Operand::Register(Reg::AX)));
             }
         }
+    }
+
+    for (i, arg) in register_args.iter().enumerate().rev() {
+        let assembly_arg = (*arg).clone().into();
+        instructions.push(Instruction::Mov {
+            src: assembly_arg,
+            dst: i.into(),
+        });
     }
 
     instructions.push(Instruction::Call(name));
